5bdaae7ec25d47fb14f64231bfebe5044cfddc60
omar fawaz <omar@example.com>
2025-01-31 01:39:45 +0200

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:medical_app/core/utils/theme_manager.dart';
-import 'package:medical_app/features/splash/presentation/views/case_details_view.dart';
 import 'package:medical_app/features/splash/presentation/views/cases_view.dart';
 
 void main() {
